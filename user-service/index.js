1413f42486bb10ca56ec8bb7ae852a9c924dd2a0
nhatthang270404@gmail.com <nhatthang270404@gmail.com>
2024-10-30 16:21:40 +0700

    cancel authen

--- user-service/index.js
+++ user-service/index.js
@@ -1,28 +1,25 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
-const setupSwaggerDocs = require('./config/swagger');
+const cookieParser = require("cookie-parser");
+const setupSwaggerDocs = require("./config/swagger");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 const corsOptions = {
-   origin: [
-       'http://localhost:5173',
-   ],
-   credentials: true,
+  origin: ["http://localhost:5173"],
+  credentials: true,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/api', require('./router/user'));
+app.use("/api", require("./router/user"));
 setupSwaggerDocs(app);
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-   console.log(`User service listening on 127.0.0.1:${port}`);
-})
-
+  console.log(`User service listening on 127.0.0.1:${port}`);
+});
