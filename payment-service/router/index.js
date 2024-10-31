85fcb09963be528af0a65751321cb60a316c81d7
Linh Võ <linhvg19804@gmail.com>
2024-10-31 11:44:37 +0700

    update: prettier formated

--- payment-service/router/index.js
+++ payment-service/router/index.js
@@ -15,28 +15,10 @@ router.post("/callback", controller.callback);
 router.post("/reload-wallet", authenticate, controller.reloadWallet);
 
 // Role-based access control for Internal Service
-router.get(
-  "/internal/get-wallet-balance",
-  authenticate,
-  verifyRole(["Internal Service"]),
-  controller.getAllWalletBalance
-);
-router.get(
-  "/internal/get-wallet-balance/:UserId",
-  controller.getWalletBalanceByUserId
-);
-router.get(
-  "/internal/get-transaction-history",
-  authenticate,
-  verifyRole(["Internal Service"]),
-  controller.getAllTransactionHistory
-);
-router.get(
-  "/internal/get-transaction-history/:UserId",
-  authenticate,
-  verifyRole(["Internal Service"]),
-  controller.getTransactionHistoryByUserId
-);
+router.get("/internal/get-wallet-balance", authenticate, verifyRole(["Internal Service"]), controller.getAllWalletBalance);
+router.get("/internal/get-wallet-balance/:UserId", controller.getWalletBalanceByUserId);
+router.get("/internal/get-transaction-history", authenticate, verifyRole(["Internal Service"]),controller.getAllTransactionHistory);
+router.get("/internal/get-transaction-history/:UserId", authenticate, verifyRole(["Internal Service"]),controller.getTransactionHistoryByUserId);
 
 router.post('/internal/payment/:UserId', authenticate, verifyRole(['Internal Service']), controller.internalPayment);
 
