3bf4e32101d74e3546aed42e9b738f9eed7e1091
Linh Võ <linhvg19804@gmail.com>
2024-11-09 01:48:19 +0700

    update: axios to auction

--- payment-service/controller/index.js
+++ payment-service/controller/index.js
@@ -1,4 +1,4 @@
-const axios = require('axios').default; // npm install axios
+const axios = require('axios'); // npm install axios
 const CryptoJS = require('crypto-js'); // npm install crypto-js
 const moment = require('moment'); // npm install moment
 const qs = require('qs'); // npm install qs
@@ -11,57 +11,57 @@ const TransactionStatus = require('../models/transaction-status');
 const BreederDetail = require('../../user-service/models/breeder');
 
 const deposit = async (req, res) => {
-  const { Amount } = req.body;
-  const { UserId } = req.user;
+   const { Amount } = req.body;
+   const { UserId } = req.user;
 
-  const config = {
-    app_id: "2553",
-    key1: "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
-    key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
-    endpoint: "https://sb-openapi.zalopay.vn/v2/create",
-  };
+   const config = {
+      app_id: "2553",
+      key1: "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
+      key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
+      endpoint: "https://sb-openapi.zalopay.vn/v2/create",
+   };
 
-  const embed_data = {
-    redirecturl: "localhost:5173",
-  };
-  const items = [{}];
-  const transID = Math.floor(Math.random() * 1000000);
-  const app_trans_id = `${moment().format("YYMMDD")}_${transID}`;
-  const order = {
-    app_id: config.app_id,
-    app_trans_id: app_trans_id,
-    app_user: "user123",
-    app_time: Date.now(),
-    item: JSON.stringify(items),
-    embed_data: JSON.stringify(embed_data),
-    amount: Amount,
-    description: "Thanh toán hóa đơn",
-    bank_code: "zalopayapp",
-    callback_url:
-      "https://4514-118-71-221-28.ngrok-free.app/payment-service/callback",
-  };
+   const embed_data = {
+      redirecturl: "localhost:5173",
+   };
+   const items = [{}];
+   const transID = Math.floor(Math.random() * 1000000);
+   const app_trans_id = `${moment().format("YYMMDD")}_${transID}`;
+   const order = {
+      app_id: config.app_id,
+      app_trans_id: app_trans_id,
+      app_user: "user123",
+      app_time: Date.now(),
+      item: JSON.stringify(items),
+      embed_data: JSON.stringify(embed_data),
+      amount: Amount,
+      description: "Thanh toán hóa đơn",
+      bank_code: "zalopayapp",
+      callback_url:
+         "https://4514-118-71-221-28.ngrok-free.app/payment-service/callback",
+   };
 
-  console.log(`app_trans_id = ${app_trans_id}`);
+   console.log(`app_trans_id = ${app_trans_id}`);
 
-  const data =
-    config.app_id +
-    "|" +
-    order.app_trans_id +
-    "|" +
-    order.app_user +
-    "|" +
-    order.amount +
-    "|" +
-    order.app_time +
-    "|" +
-    order.embed_data +
-    "|" +
-    order.item;
-  order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+   const data =
+      config.app_id +
+      "|" +
+      order.app_trans_id +
+      "|" +
+      order.app_user +
+      "|" +
+      order.amount +
+      "|" +
+      order.app_time +
+      "|" +
+      order.embed_data +
+      "|" +
+      order.item;
+   order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
 
-  const response = await axios.post(config.endpoint, null, { params: order });
+   const response = await axios.post(config.endpoint, null, { params: order });
 
-  const wallet = await Wallet.findOne({ where: { UserId: UserId } });
+   const wallet = await Wallet.findOne({ where: { UserId: UserId } });
 
    try {
       await Transaction.create({
@@ -79,95 +79,95 @@ const deposit = async (req, res) => {
       console.log(err);
    }
 
-  res.status(200).json(response.data);
+   res.status(200).json(response.data);
 };
 
 const callback = async (req, res) => {
-  const config = {
-    key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
-  };
+   const config = {
+      key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
+   };
 
-  let result = {};
+   let result = {};
 
-  try {
-    let dataStr = req.body.data;
-    let reqMac = req.body.mac;
+   try {
+      let dataStr = req.body.data;
+      let reqMac = req.body.mac;
 
-    console.log("data =", dataStr);
+      console.log("data =", dataStr);
 
-    let mac = CryptoJS.HmacSHA256(dataStr, config.key2).toString();
-    console.log("mac =", mac);
+      let mac = CryptoJS.HmacSHA256(dataStr, config.key2).toString();
+      console.log("mac =", mac);
 
-    // kiểm tra callback hợp lệ (đến từ ZaloPay server)
-    if (reqMac !== mac) {
-      // callback không hợp lệ
-      result.return_code = -1;
-      result.return_message = "mac not equal";
-    } else {
-      // thanh toán thành công
-      // merchant cập nhật trạng thái cho đơn hàng
-      let dataJson = JSON.parse(dataStr, config.key2);
-      console.log(
-        "update order's status = success where app_trans_id =",
-        dataJson["app_trans_id"]
-      );
+      // kiểm tra callback hợp lệ (đến từ ZaloPay server)
+      if (reqMac !== mac) {
+         // callback không hợp lệ
+         result.return_code = -1;
+         result.return_message = "mac not equal";
+      } else {
+         // thanh toán thành công
+         // merchant cập nhật trạng thái cho đơn hàng
+         let dataJson = JSON.parse(dataStr, config.key2);
+         console.log(
+            "update order's status = success where app_trans_id =",
+            dataJson["app_trans_id"]
+         );
 
-      result.return_code = 1;
-      result.return_message = "success";
+         result.return_code = 1;
+         result.return_message = "success";
 
-      const appTransId = dataJson["app_trans_id"];
+         const appTransId = dataJson["app_trans_id"];
 
-      const transaction = await Transaction.findOne({
-        where: { AppTransId: appTransId },
-      });
-      if (transaction) {
-        try {
-          await Transaction.update(
-            { StatusId: 2 },
-            { where: { AppTransId: appTransId } }
-          );
-          await Wallet.update(
-            { Balance: transaction.BalanceAfter },
-            { where: { WalletId: transaction.WalletId } }
-          );
-        } catch (err) {
-          console.log(err);
-        }
+         const transaction = await Transaction.findOne({
+            where: { AppTransId: appTransId },
+         });
+         if (transaction) {
+            try {
+               await Transaction.update(
+                  { StatusId: 2 },
+                  { where: { AppTransId: appTransId } }
+               );
+               await Wallet.update(
+                  { Balance: transaction.BalanceAfter },
+                  { where: { WalletId: transaction.WalletId } }
+               );
+            } catch (err) {
+               console.log(err);
+            }
+         }
       }
-    }
-  } catch (ex) {
-    result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
-    result.return_message = ex.message;
-  }
+   } catch (ex) {
+      result.return_code = 0; // ZaloPay server sẽ callback lại (tối đa 3 lần)
+      result.return_message = ex.message;
+   }
 
-  // thông báo kết quả cho ZaloPay server
-  res.status(200).json(result);
+   // thông báo kết quả cho ZaloPay server
+   res.status(200).json(result);
 };
 
 const checkOrderStatus = async (orderId) => {
-  const config = {
-    app_id: "2553",
-    key1: "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
-    key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
-    endpoint: "https://sb-openapi.zalopay.vn/v2/query",
-  };
+   const config = {
+      app_id: "2553",
+      key1: "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
+      key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
+      endpoint: "https://sb-openapi.zalopay.vn/v2/query",
+   };
 
-  let postData = {
-    app_id: config.app_id,
-    app_trans_id: orderId, // Input your app_trans_id
-  };
+   let postData = {
+      app_id: config.app_id,
+      app_trans_id: orderId, // Input your app_trans_id
+   };
 
-  let data = postData.app_id + "|" + postData.app_trans_id + "|" + config.key1; // appid|app_trans_id|key1
-  postData.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+   let data = postData.app_id + "|" + postData.app_trans_id + "|" + config.key1; // appid|app_trans_id|key1
+   postData.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
 
-  const response = await axios.post(config.endpoint, null, {
-    params: postData,
-  });
-  return response.data;
+   const response = await axios.post(config.endpoint, null, {
+      params: postData,
+   });
+   return response.data;
 };
 
 const refreshWallet = async (UserId) => {
-  const wallet = await Wallet.findOne({ where: { UserId: UserId } });
+   const wallet = await Wallet.findOne({ where: { UserId: UserId } });
 
    const transaction = await Transaction.findAll({
       where: {
@@ -177,7 +177,7 @@ const refreshWallet = async (UserId) => {
       }
    });
 
-  let balance = Number(wallet.Balance);
+   let balance = Number(wallet.Balance);
 
    for (const trans of transaction) {
       try {
@@ -200,10 +200,10 @@ const refreshWallet = async (UserId) => {
       }
    }
 
-  await Wallet.update(
-    { Balance: balance },
-    { where: { WalletId: wallet.WalletId } }
-  );
+   await Wallet.update(
+      { Balance: balance },
+      { where: { WalletId: wallet.WalletId } }
+   );
 
    return {
       WalletId: wallet.WalletId,
@@ -214,14 +214,23 @@ const refreshWallet = async (UserId) => {
 }
 
 const getWalletBalance = async (req, res) => {
-  const { UserId } = req.user;
+   const { UserId } = req.user;
 
    const refresh = await refreshWallet(UserId);
    if (refresh.status == 500) {
       return res.status(500).json({ message: "Internal Server Error" });
    }
 
-  res.status(200).json({ balance: refresh.Balance });
+   try {
+      await axios.post("http://localhost:3002/api/bid/update-balance", {
+         Amount: refresh.Balance,
+         BidderId: UserId,
+      });
+   } catch (err) {
+      console.log(err);
+   }
+
+   res.status(200).json({ balance: refresh.Balance });
 };
 
 const getTransactionHistory = async (req, res) => {
@@ -272,16 +281,16 @@ const payment = async (req, res) => {
    const { Amount, SoldLotId } = req.body;
    const { UserId } = req.user;
 
-  if (!Amount) return res.status(400).json({ message: "Amount is required" });
-  if (isNaN(Amount))
-    return res.status(400).json({ message: "Amount must be a number" });
-  if (Amount <= 0)
-    return res.status(400).json({ message: "Amount must be greater than 0" });
+   if (!Amount) return res.status(400).json({ message: "Amount is required" });
+   if (isNaN(Amount))
+      return res.status(400).json({ message: "Amount must be a number" });
+   if (Amount <= 0)
+      return res.status(400).json({ message: "Amount must be greater than 0" });
 
-  const wallet = await Wallet.findOne({ where: { UserId: UserId } });
+   const wallet = await Wallet.findOne({ where: { UserId: UserId } });
 
-  if (wallet.Balance < Amount)
-    return res.status(400).json({ message: "Not enough money" });
+   if (wallet.Balance < Amount)
+      return res.status(400).json({ message: "Not enough money" });
 
    try {
       await Transaction.create({
@@ -296,46 +305,46 @@ const payment = async (req, res) => {
          CreatedAt: Date.now(),
       });
 
-    await Wallet.update(
-      { Balance: wallet.Balance - Amount },
-      { where: { WalletId: wallet.WalletId } }
-    );
-  } catch (err) {
-    console.log(err);
-  }
+      await Wallet.update(
+         { Balance: wallet.Balance - Amount },
+         { where: { WalletId: wallet.WalletId } }
+      );
+   } catch (err) {
+      console.log(err);
+   }
 
-  res.status(200).json({ message: "Payment successfully" });
+   res.status(200).json({ message: "Payment successfully" });
 };
 
 const getAllWalletBalance = async (req, res) => {
-  let wallets = await Wallet.findAll();
-  await Promise.all(
-    wallets.map(async (wallet) => {
-      const refresh = await refreshWallet(wallet.UserId);
-      wallet.Balance = refresh.Balance;
-    })
-  );
+   let wallets = await Wallet.findAll();
+   await Promise.all(
+      wallets.map(async (wallet) => {
+         const refresh = await refreshWallet(wallet.UserId);
+         wallet.Balance = refresh.Balance;
+      })
+   );
 
-  res.status(200).json(wallets);
+   res.status(200).json(wallets);
 };
 
 const getWalletBalanceByUserId = async (req, res) => {
-  const { UserId } = req.params;
+   const { UserId } = req.params;
 
-  try {
-    // const wallet = await Wallet.findOne({ where: { UserId: UserId } });
-    const refresh = await refreshWallet(UserId);
+   try {
+      // const wallet = await Wallet.findOne({ where: { UserId: UserId } });
+      const refresh = await refreshWallet(UserId);
 
-    res.status(200).json({
-      UserId: UserId,
-      WalletId: refresh.WalletId,
-      Balance: refresh.Balance,
-      Currency: refresh.Currency,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
+      res.status(200).json({
+         UserId: UserId,
+         WalletId: refresh.WalletId,
+         Balance: refresh.Balance,
+         Currency: refresh.Currency,
+      });
+   } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Internal Server Error" });
+   }
 };
 
 const getAllTransactionHistory = async (req, res) => {
@@ -346,7 +355,7 @@ const getAllTransactionHistory = async (req, res) => {
 
    const TransTypeId = transactionTypes.find((type) => type.TransTypeName == TransType)?.TransTypeId;
    const StatusId = transactionStatus.find((status) => status.TransStatusName == Status)?.TransStatusId;
-   
+
    let transactions = await Transaction.findAll({
       where: {
          TransTypeId: TransTypeId || { [Op.ne]: null },
@@ -376,8 +385,8 @@ const getTransactionHistoryByUserId = async (req, res) => {
    const { UserId } = req.params;
    const { Status, TransType } = req.body;
 
-  try {
-    const wallet = await Wallet.findOne({ where: { UserId: UserId } });
+   try {
+      const wallet = await Wallet.findOne({ where: { UserId: UserId } });
 
       if (!wallet) return res.status(404).json({ message: "User not found" });
 
@@ -388,7 +397,7 @@ const getTransactionHistoryByUserId = async (req, res) => {
 
       const TransTypeId = transactionTypes.find((type) => type.TransTypeName == TransType)?.TransTypeId;
       const StatusId = transactionStatus.find((status) => status.TransStatusName == Status)?.TransStatusId;
-      
+
       let transactions = await Transaction.findAll({
          where: {
             WalletId: wallet.WalletId,
@@ -396,9 +405,9 @@ const getTransactionHistoryByUserId = async (req, res) => {
             StatusId: StatusId || { [Op.ne]: null }
          }
       });
-      
+
       let result = [];
-      await Promise.all(transactions.map(async(transaction) => {
+      await Promise.all(transactions.map(async (transaction) => {
          const wallet = await Wallet.findByPk(transaction.WalletId);
          result.push({
             TransId: transaction.TransId,
@@ -419,25 +428,25 @@ const getTransactionHistoryByUserId = async (req, res) => {
 }
 
 const internalPayment = async (req, res) => {
-  console.log(`req.body = ${JSON.stringify(req.body)}`);
-  const { amount, soldLotId } = req.body;
-  const Amount = Number(amount);
-  const { UserId } = req.params;
-  console.log("amount = ", amount);
-  console.warn("UserId = ", UserId);
-  console.warn("Amount = ", Amount);
-  if (!UserId) return res.status(400).json({ message: "UserId is required" });
-  if (!Amount) return res.status(400).json({ message: "Amount is required" });
-  if (isNaN(UserId))
-    return res.status(400).json({ message: "UserId must be a number" });
-  if (isNaN(Amount))
-    return res.status(400).json({ message: "Amount must be a number" });
-  if (Amount <= 0)
-    return res.status(400).json({ message: "Amount must be greater than 0" });
+   console.log(`req.body = ${JSON.stringify(req.body)}`);
+   const { amount, soldLotId } = req.body;
+   const Amount = Number(amount);
+   const { UserId } = req.params;
+   console.log("amount = ", amount);
+   console.warn("UserId = ", UserId);
+   console.warn("Amount = ", Amount);
+   if (!UserId) return res.status(400).json({ message: "UserId is required" });
+   if (!Amount) return res.status(400).json({ message: "Amount is required" });
+   if (isNaN(UserId))
+      return res.status(400).json({ message: "UserId must be a number" });
+   if (isNaN(Amount))
+      return res.status(400).json({ message: "Amount must be a number" });
+   if (Amount <= 0)
+      return res.status(400).json({ message: "Amount must be greater than 0" });
 
-  const wallet = await Wallet.findOne({ where: { UserId: UserId } });
-  if (wallet.Balance < Amount)
-    return res.status(400).json({ message: "Not enough money" });
+   const wallet = await Wallet.findOne({ where: { UserId: UserId } });
+   if (wallet.Balance < Amount)
+      return res.status(400).json({ message: "Not enough money" });
 
    try {
 
@@ -453,10 +462,10 @@ const internalPayment = async (req, res) => {
          CreatedAt: Date.now(),
       });
 
-    await Wallet.update(
-      { Balance: wallet.Balance - Amount },
-      { where: { WalletId: wallet.WalletId } }
-    );
+      await Wallet.update(
+         { Balance: wallet.Balance - Amount },
+         { where: { WalletId: wallet.WalletId } }
+      );
 
       return res.status(200).json({ message: "Payment successfully" });
    } catch (err) {
@@ -545,13 +554,13 @@ const updateUserWithdrawStatusById = async (req, res) => {
    if (!UserId) return res.status(400).json({ message: "UserId is required" });
    if (!Id) return res.status(400).json({ message: "Id is required" });
    if (!Status) return res.status(400).json({ message: "Status is required" });
-   
+
    try {
       const wallet = await Wallet.findOne({ where: { UserId: UserId } });
-   
+
       if (!wallet) return res.status(404).json({ message: "User not found" });
 
-      const transaction = await Transaction.findOne({ where: { WalletId : wallet.WalletId, TransId: Id } });
+      const transaction = await Transaction.findOne({ where: { WalletId: wallet.WalletId, TransId: Id } });
       if (!transaction) return res.status(404).json({ message: "Transaction not found" });
       if (transaction.TransTypeId != 1) return res.status(400).json({ message: "Transaction is not withdraw" });
       if (transaction.StatusId != 1) return res.status(400).json({ message: "Transaction is not pending" });
